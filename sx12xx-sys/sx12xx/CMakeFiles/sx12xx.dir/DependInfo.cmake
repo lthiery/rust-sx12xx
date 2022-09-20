b8bad57a90377eb7ad56cd7fe148e22e925e133e
lthiery <thiery.louis@gmail.com>
2022-09-20 09:39:55 -0700

    wip

---
+++ sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/board.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/board.c.o"
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/sx126x/radio.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/sx126x/radio.c.o"
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/sx126x/sx126x-board.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/sx126x/sx126x-board.c.o"
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/sx126x/sx126x.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/sx126x/sx126x.c.o"
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/sx1272/sx1272-board.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/sx1272/sx1272-board.c.o"
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/sx1272/sx1272.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/sx1272/sx1272.c.o"
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/sx1276/sx1276-board.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/sx1276/sx1276-board.c.o"
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/sx1276/sx1276.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/sx1276/sx1276.c.o"
+  "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/sx12xx.c" "/home/louis/rust-sx12xx/sx12xx-sys/sx12xx/CMakeFiles/sx12xx.dir/sx12xx.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
